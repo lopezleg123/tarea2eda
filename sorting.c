0c54445a24f6d70accbab8288929f6711768dd81
Lopez <lopez@anon.anon.com>
2022-06-27 14:06:47 -0400

    xd

---
+++ sorting.c
@@ -0,0 +1,28 @@
+#include "sorting.h"
+
+int main () {   
+
+    FILE * archivo;     
+    char temp [1750];
+    int numeroLineas;
+
+    archivo = fopen("tweets.csv" , "r");
+        
+    
+    while (!feof(archivo)) //CONTAMOS EL NUMERO DE LINEAS.
+    {   
+        fgets(temp,1750,archivo);
+        numeroLineas++;
+    }
+    
+    rewind(archivo);
+
+    printf("%d" , numeroLineas);
+    
+    tweet tweets[numeroLineas];
+
+    fclose(archivo);
+
+    return 0;
+
+}
